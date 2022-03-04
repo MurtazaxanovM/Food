2da4cb5988145d547b1173c9d4955ad16bff2462
hp <hp@DESKTOP-HSBP6MK>
2022-03-04 17:57:30 +0500

    Добавьте файлы проекта.

---
+++ Food/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Food.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Food.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            ViewData["ComName"] = "RTM Margilan";
+            ViewBag.Header = "Discover Restuarant And Food";
+
+            List<FoodModel> food = new List<FoodModel>()
+            {
+                new FoodModel()
+                {
+                    Id =1,
+                    Name = "Breakfast",
+                    Image = "~/images/r1.jpg"
+                },
+
+                new FoodModel()
+                {
+                    Id =1,
+                    Name = "Breakfast",
+                    Image = "~/images/r2.jpg"
+                },
+
+                new FoodModel()
+                {
+                    Id =1,
+                    Name = "Breakfast",
+                    Image = "~/images/r3.jpg"
+                },
+
+                new FoodModel()
+                {
+                    Id =1,
+                    Name = "Breakfast",
+                    Image = "~/images/r1.jpg"
+                },
+
+                new FoodModel()
+                {
+                    Id =1,
+                    Name = "Breakfast",
+                    Image = "~/images/r2.jpg"
+                },
+
+                new FoodModel()
+                {
+                    Id =1,
+                    Name = "Breakfast",
+                    Image = "~/images/r3.jpg"
+                }
+            };
+            return View(food);
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+    }
+}
---
+++ Food/Models/FoodModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Food.Models
+{
+    public class FoodModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
